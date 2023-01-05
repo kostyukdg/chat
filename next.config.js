52df9fc200984c2d8504f3d9e37ddeb8342c74e0
kostyukdg <kostyukdg@gmail.com>
2023-01-05 23:19:19 +0300

    Fix styled components ssr

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: {
+    styledComponents: { ssr: true },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
